308952f72c03a5bae0cbaaef5e225772105946e0
Edward Suwandi <148249720+iwakkk@users.noreply.github.com>
2026-01-09 20:05:49 +0700

    Initial Commit

---
+++ LSP Certification/LSP_CertificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LSP_CertificationApp.swift
+//  LSP Certification
+//
+//  Created by Edward Suwandi on 09/01/26.
+//
+
+import SwiftUI
+
+@main
+struct LSP_CertificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LSP CertificationTests/LSP_CertificationTests.swift
@@ -0,0 +1,17 @@
+//
+//  LSP_CertificationTests.swift
+//  LSP CertificationTests
+//
+//  Created by Edward Suwandi on 09/01/26.
+//
+
+import Testing
+@testable import LSP_Certification
+
+struct LSP_CertificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
